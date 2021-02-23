c1e166ec2325d2c307969c25b23b241d99998500
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-02-23 17:20:50 +0300

    🔥 Enhance location icon

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -5,7 +5,7 @@ import {contactInfo} from "../../portfolio";
 import emoji from "react-easy-emoji";
 import {Fade} from "react-reveal";
 
-export default function GithubProfileCard({prof}) {
+export default function GithubProfileCard({prof, isDark}) {
   if (prof.hireable !== null) {
     prof.hireable = "Yes";
   } else {
@@ -24,7 +24,7 @@ export default function GithubProfileCard({prof}) {
             {prof.location !== null && (
               <div className="location-div">
                 <span className="desc-prof">
-                  <svg
+                  {/* <svg
                     viewBox="0 0 12 16"
                     version="1.1"
                     width="20"
@@ -34,6 +34,28 @@ export default function GithubProfileCard({prof}) {
                     <path
                       fillRule="evenodd"
                       d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"
+                    ></path>
+                  </svg> */}
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="location-arrow"
+                    className={`svg-inline--fa fa-location-arrow fa-w-16 octicon location-svg ${
+                      isDark ? "location-svg-dark" : "location-svg-light"
+                    }`}
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 12 16"
+                    width="14"
+                    height="20"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M444.52 3.52L28.74 195.42c-47.97 22.39-31.98 92.75 19.19 92.75h175.91v175.91c0 51.17 70.36 67.17 92.75 19.19l191.9-415.78c15.99-38.39-25.59-79.97-63.97-63.97z"
                     ></path>
                   </svg>
                   {prof.location}
