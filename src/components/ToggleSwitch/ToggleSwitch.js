6a12fd24081be2059e3de78716d21844f9e11662
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-02-23 15:30:19 +0300

    🎉 Custom Dark mode switch

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,24 +1,38 @@
 import React, {useState, useContext} from "react";
+import Toggle from "react-toggle";
 import StyleContext from "../../contexts/StyleContext";
-import "./ToggleSwitch.css";
+// import styles from  "./ToggleSwitch.css";
+import  "./ToggleSwitch.css";
+
+import "react-toggle/style.css"
 
 const ToggleSwitch = () => {
   const {isDark} = useContext(StyleContext);
   const [isChecked, setChecked] = useState(isDark);
   const styleContext = useContext(StyleContext);
 
+  const Dark = ({icon, style}) => (
+    <span className={"toggle dark"} style={style}>
+      {icon}
+    </span>
+  );
+  const Light = ({icon, style}) => (
+    <span className={"toggle light"} style={style}>
+      {icon}
+    </span>
+  );
+
   return (
-    <label className="switch">
-      <input
-        type="checkbox"
-        checked={isDark}
-        onChange={() => {
-          styleContext.changeTheme();
-          setChecked(!isChecked);
-        }}
-      />
-      <span className="slider round"></span>
-    </label>
+    <Toggle
+      icons={{
+        checked: <Dark icon={"🌜"} />,
+        unchecked: <Light icon={"🌞"} />
+      }}
+      onChange={() => {
+        styleContext.changeTheme();
+        setChecked(!isChecked);
+      }}
+    />
   );
 };
 export default ToggleSwitch;
